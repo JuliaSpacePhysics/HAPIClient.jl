9898f73fdd547be6743201e2ca0d813192d20b4e
Beforerr <zzj956959688@gmail.com>
2025-03-31 21:59:23 -0700

    feat: load Server from json file

--- src/HAPIClient.jl
+++ src/HAPIClient.jl
@@ -10,7 +10,6 @@ import Base: getproperty, propertynames, getindex, show
 import Unitful: unit
 
 export hapi, get_data
-export CDAWeb, CSA
 export HAPIVariable, HAPIVariables
 
 include("server.jl")
@@ -26,5 +25,16 @@ hapi(server) = get_catalog(server)
 hapi(server, dataset) = get_parameters(server, dataset)
 hapi(server, dataset, parameters) = get_parameters(server, dataset, parameters)
 hapi(server, dataset, parameters, tmin, tmax; kwargs...) = get_data(server, dataset, parameters, tmin, tmax; kwargs...)
+
+function __init__()
+    ccall(:jl_generating_output, Cint, ()) == 1 && return nothing
+    load_servers_from_json(; register=true)
+    # export servers
+    foreach(values(SERVERS)) do value
+        sym = Symbol(value.id)
+        @eval const $sym = $value
+        @eval export $sym
+    end
+end
 
 end
--- src/server.jl
+++ src/server.jl
@@ -1,5 +1,8 @@
 import Base: string
 
+const DEFAULT_SERVERS_JSON_URL = "https://raw.githubusercontent.com/hapi-server/servers/refs/heads/master/servers.json"
+const DEFAULT_FORMAT = "csv"
+
 abstract type AbstractServer end
 
 @kwdef struct Server <: AbstractServer
@@ -7,19 +10,15 @@ abstract type AbstractServer end
     id::String
     title::String
     HAPI::Union{String,Nothing} = nothing
-    format::String = "json"
+    format::String = DEFAULT_FORMAT
 end
 
-CDAWeb = Server(; url="https://cdaweb.gsfc.nasa.gov/hapi", id="CDAWeb", title="CDAWeb")
-CSA = Server(; url="https://csatools.esac.esa.int/HapiServer/hapi", id="CSA", title="Cluster Science Archive", HAPI="3.2", format="csv")
+# CSA = Server(; url="https://csatools.esac.esa.int/HapiServer/hapi", id="CSA", title="Cluster Science Archive", HAPI="3.2", format="csv")
 
 # Define available servers
 const SERVERS = Dict{String,Server}()
 
 register_server!(server::Server) = (SERVERS[uppercase(server.id)] = server)
-# Register default servers
-register_server!(CDAWeb)
-register_server!(CSA)
 
 """Get a HAPI server instance by its ID."""
 Server(id) = SERVERS[uppercase(id)]
@@ -27,8 +26,7 @@ Server(id) = SERVERS[uppercase(id)]
 Base.string(s::Server) = getfield(s, :url)
 format(s::Server) = getfield(s, :format)
 """Default format for HAPI servers."""
-format(s) = "json"
-
+format(s) = DEFAULT_FORMAT
 
 """
     get_capabilities(server) -> Dict
@@ -39,4 +37,30 @@ function get_capabilities(server)
     url = string(server) * "/capabilities"
     response = HTTP.get(url)
     return JSON.parse(String(response.body))
+end
+
+"""
+    load_servers_from_json(; url=DEFAULT_SERVERS_JSON_URL, register=false)
+
+Load HAPI servers from a JSON file at the specified URL.
+"""
+function load_servers_from_json(; url=DEFAULT_SERVERS_JSON_URL, register=false)
+    # Fetch the JSON data
+    response = HTTP.get(url)
+    servers_data = JSON.parse(String(response.body))
+
+    # Register each server
+    servers = map(servers_data["servers"]) do server_info
+        Server(
+            url=server_info["url"],
+            id=server_info["id"],
+            title=server_info["title"],
+            HAPI=get(server_info, "HAPI", nothing),
+            format=get(server_info, "format", DEFAULT_FORMAT)
+        )
+    end
+
+    register && register_server!.(servers)
+
+    return servers
 end
